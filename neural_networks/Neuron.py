8025e66a7e7009c53ff2e0370dbf997e64a49b17
Jake <jakemckenna1025@gmail.com>
2026-09-06 01:25:52 -0400

    implemented backpropogation/training

--- neural_networks/Neuron.py
+++ neural_networks/Neuron.py
@@ -5,9 +5,16 @@ class Neuron:
     def __init__(self, weights:np.typing.ArrayLike, bias:float):
         self.weights = weights
         self.bias = bias
+        self.last_inputs = None
+        self.last_weighted_sum = None
+        self.last_activation = None
+        self.delta = None
 
     def weighted_sum(self, inputs: np.typing.ArrayLike):
-        return float(np.sum(inputs * self.weights) + self.bias)
+        self.last_weighted_sum = float(np.sum(inputs * self.weights) + self.bias)
+        return self.last_weighted_sum
 
     def activation(self, inputs:float):
-        return 1 / (1 + math.exp(-self.weighted_sum(inputs)))
+        self.last_inputs = inputs
+        self.last_activation = 1 / (1 + math.exp(-self.weighted_sum(inputs)))
+        return self.last_activation
